cb2ddc6527b6f5bd306f0db1d4c177646f04ae47
jurchello <devworkit@gmail.com>
2026-03-20 10:59:59 +0200

    Clarify monolog logger type for PHPStan

--- app/Infrastructure/Logging/JsonLogTap.php
+++ app/Infrastructure/Logging/JsonLogTap.php
@@ -7,12 +7,16 @@ namespace App\Infrastructure\Logging;
 use Illuminate\Log\Logger as IlluminateLogger;
 use Monolog\Formatter\JsonFormatter;
 use Monolog\Handler\FormattableHandlerInterface;
+use Monolog\Logger as MonologLogger;
 
 final class JsonLogTap
 {
     public function __invoke(IlluminateLogger $logger): void
     {
-        foreach ($logger->getLogger()->getHandlers() as $handler) {
+        /** @var MonologLogger $monolog */
+        $monolog = $logger->getLogger();
+
+        foreach ($monolog->getHandlers() as $handler) {
             if ($handler instanceof FormattableHandlerInterface) {
                 $handler->setFormatter(new JsonFormatter);
             }
